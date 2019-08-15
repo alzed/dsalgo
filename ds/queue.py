533dda920d4c75bebf749490a77bdfb623db5ca2
Madhumitha P Ravishankar <madhumithap.ravishankar@gmail.com>
2019-08-15 18:53:10 +0530

    Create queue.py

---
+++ ds/queue.py
@@ -0,0 +1,23 @@
+class Queue:
+    'Queue class'
+    def __init__(self):
+        self.queue = []
+    
+    def to_array(self):
+        return list(self.queue)
+
+    def enque(self, value):
+        self.queue.append(value)
+
+    def deque(self):
+        self.queue.pop(0)
+
+    def __len__(self):
+        return len(self.queue)
+    
+    def first(self):
+        return self.queue[0]
+
+    def last(self):
+        return self.queue[-1]
+    
