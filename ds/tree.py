1b8b2e2cdc55a711e5dfd1ca1f233400fc08436b
Madhumitha P Ravishankar <madhumithap.ravishankar@gmail.com>
2019-12-10 23:22:48 +0530

    Update tree.py
    Add remove, to_array methods

--- ds/tree.py
+++ ds/tree.py
@@ -1,60 +1,128 @@
 class BinaryNode:
-    def __init__(self, data, left, right):
+    def __init__(self, data, left=None, right=None):
         self.data = data
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return str(self.data)
+        
 class BST:
     def __init__(self):
         self._root = None
-        self._size = 0
-        self._min = None
-        self._max = None
-        
-    def to_array(self):
-        return self._preorder(self._root, [])
 
     def insert(self, item):
-        self._place(item)
-        self._size += 1
+        if self._root:
+            self._root = self._insert(item, self._root)
+        else:
+            self._root = BinaryNode(item)
 
-    def remove(self, item):
-        pass
+    def _insert(self, item, node):
+        if not node:
+            return BinaryNode(item)
+        elif item < node.data:
+            node.left = self._insert(item, node.left)
+        elif item > node.data:
+            node.right = self._insert(item, node.right)
+        return node
 
     def get_root(self):
-        pass
-
-    def search(self, item):
-        pass
+        return self._root.data
 
     def min(self):
-        pass
+        node = self._root
+        while node.left:
+            node = node.left
+        return node.data
     
     def max(self):
-        pass
+        node = self._root
+        while node.right:
+            node = node.right
+        return node.data
+
+    def to_array(self, key='inorder'):
+        arr = []
+        if key == 'preorder':
+            return self._preorder(self._root, arr)
+        elif key == 'postorder':
+            return self._postorder(self._root, arr)
+        return self._inorder(self._root, arr)
+
+    def _preorder(self, node, array):
+        if not node:
+            return array
+        array.append(node.data)
+        self._preorder(node.left, array)
+        self._preorder(node.right, array)
+        return array
+
+    def _inorder(self, node, array):
+        if not node: 
+            return array
+        self._inorder(node.left, array)
+        array.append(node.data)
+        self._inorder(node.right, array)
+        return array
+
+    def _postorder(self, node, array):
+        if not node: 
+            return array
+        self._inorder(node.left, array)
+        self._inorder(node.right, array)
+        array.append(node.data)
+        return array
+
+    def __contains__(self, item):
+        return True if self._search(item, self._root) else False
+
+    def _search(self, item, root):
+        if not root:
+            return False
+        if item < root.data:
+            root = self._search(item, root.left)
+        elif item > root.data:
+            root = self._search(item, root.right)
+        return root
+
+    def remove(self, item):
+        self.delete_node(item, self._root)
+        
+    def delete_node(self, item, node):
+        if not node:
+            return node
+        elif item < node.data:
+            node.left = self.delete_node(item, node.left)
+        elif item >  node.data:
+            node.right = self.delete_node(item, node.right)
+        else:
+            if not node.left:
+                temp = node.right
+                node = None
+                return temp
+
+            if not node.right:
+                temp = node.left
+                node = None 
+                return temp
+
+            minimum = self.minimum(node.right)
+            node.data = minimum.data
+            node.right = self.delete_node(minimum.data, node.right) 
+        return node
+
+    def minimum(self, node):
+        pre = None
+        while node:
+            pre = node
+            node = node.left
+        return pre
 
     def size(self):
-        return self._size
+        pass
 
     def height(self):
         pass
 
     def is_empty(self):
-        return not self._size
-
-    def _place(self, item, node=None):
-        root = self._root if not node else node
-        if not root: 
-            root = BinaryNode(item, None, None)
-        elif item < root:
-            self._place(item, root.left)
-        elif item > root:
-            self._place(item, root.right)
-
-    def _preorder(self, node, array):
-        if not node:
-            return None
-        array.append(node.data)
-        self._preorder(node.left, array)
-        self._preorder(node.right, array)
-        return array
+        return False if self._root else True
