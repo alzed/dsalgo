4ee13595985499717c7a89655bd4baef8d1c726f
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2020-01-07 12:23:57 +0530

    Update queue.py
    Add linkedlist implementation

--- ds/queue.py
+++ ds/queue.py
@@ -1,8 +1,14 @@
+from .linkedlist import Linkedlist
+
 class Queue:
     'Queue class'
-    def __init__(self):
-        self._queue = []
-        self._length = 0
+    def __init__(self, array=None):
+        if array:
+            self._queue = array
+            self._length = len(array)
+        else:
+            self._queue = []
+            self._length = 0
     
     def to_array(self):
         return list(self._queue)
@@ -26,3 +32,36 @@ class Queue:
     
     def is_empty(self):
         return not self._length
+
+class QueueList:
+    'Queue implemented as a linked list'
+    def __init__(self, array = None):
+        self._queue = Linkedlist()
+        if array:
+            for i in array:
+                self._queue.append(i)
+        self._length = len(self._queue)
+
+    def to_array(self):
+        return self._queue.to_array()
+
+    def enqueue(self, item):
+        self._queue.append(item)
+        self._length += 1
+
+    def dequeue(self):
+        self._length -= 1
+        return self._queue.shift()
+
+    def __len__(self):
+        return self._length
+    
+    def first(self):
+        return self._queue[0]
+
+    def last(self):
+        return self._queue[-1]
+    
+    def is_empty(self):
+        return self._queue.is_empty()
+        
