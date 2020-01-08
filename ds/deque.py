2585a5706be4e0f8b0824b2d16e6145228b0797a
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2020-01-08 20:36:48 +0530

    Update deque.py
    Add linkedlist implementation

--- ds/deque.py
+++ ds/deque.py
@@ -1,3 +1,5 @@
+from .doubly_linkedlist import DoublyLinkedlist
+
 class Deque:
     'Deque class'
     def __init__(self):
@@ -34,3 +36,41 @@ class Deque:
     
     def is_empty(self):
         return not self._length
+        
+class DequeList:
+    'Deque class implemented using doubly linkedlist'
+    def __init__(self):
+        self._deque = DoublyLinkedlist()
+        self._length = 0
+    
+    def to_array(self):
+        return self._deque.to_array()
+
+    def enque(self, item):
+        self._deque.append(item)
+        self._length += 1
+
+    def enque_left(self, item):
+        self._deque.prepend(item)
+        self._length += 1
+    
+    def deque(self):
+        self._length -= 1
+        return self._deque.shift()
+
+    def deque_right(self):
+        self._length -= 1
+        return self._deque.pop()
+
+    def __len__(self):
+        return self._length
+    
+    def first(self):
+        return self._deque[0]
+
+    def last(self):
+        return self._deque[-1]
+    
+    def is_empty(self):
+        return not self._length
+        
