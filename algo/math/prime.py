15021a32206d81d4d9b66c381d58e706431d3970
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2020-01-27 22:17:39 +0530

    Update prime.py

--- algo/math/prime.py
+++ algo/math/prime.py
@@ -1,16 +1,3 @@
-from random import randint
-from gcd import gcd
-
-def is_prime(integer):
-    if integer <= 3:
-        return True
-    i = 2
-    while i*i <= integer:
-        if integer%i == 0:
-            return False
-        i += 1
-    return True  
-
 def sieve(integer):
     """ Seive of eratosthenes 
         return all prime numbers upto 'integer' 
@@ -29,20 +16,4 @@ def sieve(integer):
 
     return prime
 
-def fermat(integer):
-    def power(x, y, n):
-        if y == 0:
-            return 1
-        elif y%2 == 0:
-            return power((x*x)%n, y//2, n)
-        else:
-            return (x * power((x*x)%n, (n-1)//2, n))%n
-
-    for _ in range(3):
-        a = randint(2, integer-2)
-        if not gcd(integer, a) == 1:
-            return False
-        elif not power(a, integer-1, integer) == 1:
-            return False
-    return True
 
