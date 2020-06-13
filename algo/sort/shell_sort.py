cc100fdea94fe5d7df3794213a60b19f9aabcf79
alzed <madhumithap.ravishankar@gmail.com>
2020-06-13 12:29:23 +0530

    Update shell_sort.py

---
+++ algo/sort/shell_sort.py
@@ -0,0 +1,26 @@
+from copy import deepcopy
+
+def shell_sort(array):
+    arr = deepcopy(array)
+    sub_arr_len = len(array)//2
+
+    while sub_arr_len > 0:
+        for i in range(sub_arr_len):
+            gap_insertion_sort(arr, i, sub_arr_len)
+            print(i, arr)
+        sub_arr_len //= 2
+
+    return arr
+
+def gap_insertion_sort(array, start, gap):
+    arr_len = len(array)
+    for i in range(start+gap, arr_len, gap):
+        temp = array[i]
+        j = i-gap
+        while j >= 0:
+            if array[j] > temp:
+                array[i] = array[j]
+                i -= gap
+            j -= gap
+        array[i] = temp
+    
