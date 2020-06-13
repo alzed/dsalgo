d016416a9aa6836324b893d83214f52609a65c29
alzed <madhumithap.ravishankar@gmail.com>
2020-06-13 23:11:04 +0530

    Update ternary_search.py

--- algo/search/ternary_search.py
+++ algo/search/ternary_search.py
@@ -1,2 +1,20 @@
-def ternary_search(array, element):
-    pass            
+ternary_search = lambda a, e: ternary(a, 0, len(a)-1, e) 
+
+def ternary(array, start, end, element):
+    if start < end:
+        mid1 = start + (end-start)//3
+        mid2 = end - (end-start)//3
+        
+        if array[mid1] == element:
+            return mid1
+        if array[mid2] == element:
+            return mid2
+        
+        if element < array[mid1]:
+            return ternary(array, start, mid1, element)
+        elif element > array[mid2]:
+            return ternary(array, mid2, end, element)
+        else:
+            return ternary(array, mid1, mid2, element)
+
+    return None
