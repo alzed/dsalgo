4a0765b45171e6319e5bc9957a97628a8873b56f
alzed <madhumithap.ravishankar@gmail.com>
2020-06-13 13:44:47 +0530

    Update quick_sort.py
    Remove random pivot

--- algo/sort/quick_sort.py
+++ algo/sort/quick_sort.py
@@ -13,12 +13,33 @@ def quick_sort_helper(arr, start, end):
         quick_sort_helper(arr, pivot+1, end)
 
 def partition_sort(arr, start, end):
-    pivot_value = arr[randint(start, end)]
-    i = start + 1
-    j = start + 1
+    pivot = start
+    i = start+1
+    j = end
+
+    done = False
+    while not done:
+        while i <= j and arr[i] <= arr[pivot]:
+            i += 1
+        while j >= i and arr[j] >= arr[pivot]:
+            j -= 1
+    
+        if j < i:
+            done = True
+        else:
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[j], arr[pivot] = arr[pivot], arr[j]
+    
+    return j
+    
+def partition_sort_2(arr, start, end):
+    pivot = start
+    i = start+1
+    j = start+1
     
     while j <= end: 
-        if arr[j] < pivot_value:
+        if arr[j] < arr[pivot]:
             arr[i], arr[j] = arr[j], arr[i]
             i += 1
         j += 1
